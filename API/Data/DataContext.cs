4a1ac6c217eb00c3989255a15a652417cd604669
ManingerZ <maning25@yahoo.com>
2021-01-08 10:40:07 +0800

    initial commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics.CodeAnalysis;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<AppUser> Users { get; set; }
+        public DataContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+
+        protected DataContext()
+        {
+        }
+    }
+}
