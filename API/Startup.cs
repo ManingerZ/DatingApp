991bbd661066aca25f570b20f08704c11b9e793e
ManingerZ <maning25@yahoo.com>
2021-01-13 09:57:12 +0800

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,8 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+            services.AddCors(); //this will allow cors origin call between frontent and api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +53,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")); //add this code specifically at this location ()between useroutng and use auth)
 
             app.UseAuthorization();
 
