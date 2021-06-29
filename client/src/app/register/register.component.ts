f369e08d9fcff157ee38b197a4962e06ffba4c99
ManingerZ <maning25@yahoo.com>
2021-06-29 12:08:06 +0800

    end of section 6

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../services/account.service';
 
 @Component({
@@ -12,13 +13,19 @@ export class RegisterComponent implements OnInit {
   @Input() usersFromHomeComponent: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   register(){
-    this.accountService.register(this.model).subscribe(response => {console.log(response)}, error => {console.log(error)});
+    this.accountService.register(this.model).subscribe(response => {
+      console.log(response)
+    }, 
+      error => {
+        console.log(error)
+        this.toastr.error(error.error?? "missing info");
+      });
   }
 
   cancel(){this.cancelRegister.emit(false);}
